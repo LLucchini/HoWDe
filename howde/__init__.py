3690b6176ea202c632c1c32d25fc490b7ba002d3
Lorenzo Lucchini <33127266+LLucchini@users.noreply.github.com>
2025-11-21 23:31:57 +0100

    Update __init__.py
    Added version in __init__

--- howde/__init__.py
+++ howde/__init__.py
@@ -1,5 +1,14 @@
 from .pipeline import HoWDe_labelling
-
+from importlib.metadata import version, PackageNotFoundError
 import warnings
 
 warnings.filterwarnings("ignore")
+
+try:
+    # This must match [project].name in pyproject.toml
+    __version__ = version("HoWDe")
+except PackageNotFoundError:
+    # Fallback for cases where the package is not installed
+    # (e.g. running directly from source without `pip install -e .`)
+    __version__ = "0.0.0"
+
